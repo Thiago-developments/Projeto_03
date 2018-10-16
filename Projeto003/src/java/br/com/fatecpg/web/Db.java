975bfb387159aec59616270d9d123e91418117ac
Nicole <Nicole@Nicole-Note>
2018-10-15 22:22:36 -0300

    Adição da instanciação

--- Projeto003/src/java/br/com/fatecpg/web/Db.java
+++ Projeto003/src/java/br/com/fatecpg/web/Db.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Db {
     private static final ArrayList<cadastro_pessoa> pessoa = new ArrayList<>();
-    
+     private static final ArrayList<cadastro_empresa> empresa = new ArrayList<>();
     public static ArrayList<cadastro_pessoa> getDados(){
         if (pessoa.isEmpty()){
             cadastro_pessoa p1 = new cadastro_pessoa();
@@ -21,4 +21,20 @@ public class Db {
         }
         return pessoa;
     }
-}
+     public static ArrayList<cadastro_empresa> getDadosem(){
+        if (empresa.isEmpty()){
+            cadastro_empresa e1 = new cadastro_empresa();
+            e1.setDadosem("Microsoft","Microsoft Corporation","3875024013012","(11)98899-3317", "www.microsoft.com");
+            empresa.add(e1);
+           
+            cadastro_empresa e2 = new cadastro_empresa();
+            e2.setDadosem("Samsung","Samsung Electronics Co. Ltd","4556789131455","(11)99756-7186", "www.samsung.com");
+            empresa.add(e2);
+            
+            cadastro_empresa e3 = new cadastro_empresa();
+            e3.setDadosem("IBM","International Business Machines Corporation","579876543134","(11)99656-3344", "www.ibm.com");
+            empresa.add(e3);
+        }
+        return empresa;
+    }
+}    
